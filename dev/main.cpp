16dc7aa5676c73b9a90f83794dcbebf423759d2b
ysuzuki19 <msethuuh7@i.softbank.jp>
2021-03-18 23:58:48 +0900

    config functions

--- dev/main.cpp
+++ dev/main.cpp
@@ -11,22 +11,39 @@ class Cpplot {
     }
     ~Cpplot () {
       ::pclose(gp_);
+      init();
     }
     void init () {
-    }
-    void setLabels (std::string xlabel, std::string ylabel) {
-      fprintf(gp_, "set xlabel '%s'\n", xlabel.c_str());
-      fprintf(gp_, "set ylabel '%s'\n", ylabel.c_str());
-    }
-    void plot (vector<int> v) {
       fprintf(gp_, "unset key\n");
-      fprintf(gp_, "set xrange[0:%d]\n", int(v.size()));
-      fprintf(gp_, "set yrange[0:%d]\n", int(v.size()));
-      fprintf(gp_, "set zrange[-2:2]\n");
-      fprintf(gp_, "set zlabel \"Amplitude\"\n");
+      fprintf(gp_, "set terminal qt font 'Helvetica'\n");
+    }
+    void setXLabel (std::string label) {
+      fprintf(gp_, "set xlabel '%s'\n", label.c_str());
+    }
+    void setYLabel (std::string label) {
+      fprintf(gp_, "set ylabel '%s'\n", label.c_str());
+    }
+
+    void setLabels (std::string xlabel, std::string ylabel) {
+      this->setXLabel(xlabel);
+      this->setYLabel(ylabel);
+    }
+
+    void setXRange (int start, int end) {
+      fprintf(gp_, "set xrange[%d:%d]\n", start, end);
+    }
+
+    void setYRange (int start, int end) {
+      fprintf(gp_, "set yrange[%d:%d]\n", start, end);
+    }
+
+    void plot (vector<int> v) {
+      setXRange(0, v.size());
+      setYRange(0, v.size());
+      //fprintf(gp_, "set zrange[-2:2]\n");
+      //fprintf(gp_, "set zlabel \"Amplitude\"\n");
       fprintf(gp_, "plot '-' with points\n");
       for (int i=0; i<v.size(); ++i) {
-        printf("%d, %d\n", i, v[i]);
         fprintf(gp_, "%d, %d\n", i, v[i]);
       }
       fprintf(gp_, "e\n");
