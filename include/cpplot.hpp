7eb8995f5cde2d7ce4269e6add66e477c083d8d3
ysuzuki19 <ysuzuki.cpp@gmail.com>
2021-03-19 21:13:09 +0900

    cpplot.hpp

--- include/cpplot.hpp
+++ include/cpplot.hpp
@@ -1,81 +1,116 @@
 #ifndef YSL_CPPLOT
 
 #pragma once
+#include <iostream>
 #include <vector>
+#include "pipestream.hpp"
 
 class Cpplot {
   private:
-    FILE *gp_;
-
-    template <typename T>
-    const char* c_str(T num) {
-      return std::to_string(num).c_str();
-    }
+    pipestream pipe_;
 
   public:
-    Cpplot () {
-      gp_ = ::popen("gnuplot -persist", "w");
+    Cpplot () : pipe_("gnuplot -persist") {
       init();
     }
     ~Cpplot () {
-      ::pclose(gp_);
-    }
-    void init () {
-      ::fprintf(gp_, "unset key\n");
-      ::fprintf(gp_, "set terminal qt font 'Helvetica'\n");
-    }
-    void setXLabel (std::string label) {
-      ::fprintf(gp_, "set xlabel '%s'\n", label.c_str());
-    }
-    void setYLabel (std::string label) {
-      ::fprintf(gp_, "set ylabel '%s'\n", label.c_str());
-    }
-    void setZLabel (std::string label) {
-      ::fprintf(gp_, "set zlabel '%s'\n", label.c_str());
     }
 
-    void setLabels (std::string xlabel, std::string ylabel) {
-      this->setXLabel(xlabel);
-      this->setYLabel(ylabel);
-    }
+    void init ();
 
-    void setLabels (std::string xlabel, std::string ylabel, std::string zlabel) {
-      this->setXLabel(xlabel);
-      this->setYLabel(ylabel);
-      this->setZLabel(zlabel);
-    }
+    void setXLabel (const char* label);
+    void setXLabel (std::string label);
+    void setYLabel (const char* label);
+    void setYLabel (std::string label);
+    void setZLabel (const char* label);
+    void setZLabel (std::string label);
 
-    template <typename T>
-    void setXRange (T start, T finish) {
-      ::fprintf(gp_, "set xrange[%s:%s]\n", c_str(start), finish);
-    }
-    template <typename T>
-    void setYRange (T start, T finish) {
-      ::fprintf(gp_, "set yrange[%s:%s]\n", c_str(start), finish);
-    }
-    template <typename T>
-    void setZRange (T start, T finish) {
-      ::fprintf(gp_, "set zrange[%s:%s]\n", c_str(start), finish);
-    }
+    void setLabels (const char* const& xlabel, const char* const& ylabel);
+    void setLabels (std::string const& xlabel, std::string const& ylabel);
+    void setLabels (const char* const& xlabel, const char* const& ylabel, const char* const& zlabel);
+    void setLabels (std::string const& xlabel, std::string const& ylabel, std::string const& zlabel);
 
-    template <typename T>
-    void plot (std::vector<T> v) {
-      ::fprintf(gp_, "plot '-' with points\n");
-      for (int i=0; i<v.size(); ++i) {
-        ::fprintf(gp_, "%s, %s\n", c_str(i), c_str(v[i]));
-      }
-      ::fprintf(gp_, "e\n");
-      ::fflush(gp_);
-    }
+    template <typename T> void setXRange (T start, T finish);
+    template <typename T> void setYRange (T start, T finish);
+    template <typename T> void setZRange (T start, T finish);
 
-    template <typename T>
-    void plot (std::vector<std::pair<T,T>> vpair) {
-      ::fprintf(gp_, "plot '-' with points\n");
-      for (const auto& elm: vpair) {
-        ::fprintf(gp_, "%s, %s\n", c_str(elm.first), c_str(elm.second));
-      }
-      ::fprintf(gp_, "e\n");
-      ::fflush(gp_);
-    }
+    template <typename T> void plot (std::vector<T> v);
+    template <typename T> void plot (std::vector<std::pair<T,T>> vpair);
 };
+
+void Cpplot::init () {
+  pipe_ << "unset key\n";
+  pipe_ << "set terminal qt font 'Helvetica'\n";
+}
+void Cpplot::setXLabel (const char* label) {
+  pipe_ << "set xlabel '" << label << "'\n";
+}
+void Cpplot::setXLabel (std::string label) {
+  pipe_ << "set xlabel '" << label << "'\n";
+}
+void Cpplot::setYLabel (const char* label) {
+  pipe_ << "set ylabel '" << label << "'\n";
+}
+void Cpplot::setYLabel (std::string label) {
+  pipe_ << "set ylabel '" << label << "'\n";
+}
+void Cpplot::setZLabel (const char* label) {
+  pipe_ << "set zlabel '" << label << "'\n";
+}
+void Cpplot::setZLabel (std::string label) {
+  pipe_ << "set zlabel '" << label << "'\n";
+}
+
+void Cpplot::setLabels (const char* const& xlabel, const char* const& ylabel) {
+  this->setXLabel(xlabel);
+  this->setYLabel(ylabel);
+}
+void Cpplot::setLabels (std::string const& xlabel, std::string const& ylabel) {
+  this->setXLabel(xlabel);
+  this->setYLabel(ylabel);
+}
+
+void Cpplot::setLabels (const char* const& xlabel, const char* const& ylabel, const char* const& zlabel) {
+  this->setXLabel(xlabel);
+  this->setYLabel(ylabel);
+  this->setZLabel(zlabel);
+}
+void Cpplot::setLabels (std::string const& xlabel, std::string const& ylabel, std::string const& zlabel) {
+  this->setXLabel(xlabel);
+  this->setYLabel(ylabel);
+  this->setZLabel(zlabel);
+}
+
+template <typename T>
+void Cpplot::setXRange (T start, T finish) {
+  pipe_ << "set xrange[" << start << ":"<< finish << "]\n";
+}
+template <typename T>
+void Cpplot::setYRange (T start, T finish) {
+  pipe_ << "set yrange[" << start << ":"<< finish << "]\n";
+}
+template <typename T>
+void Cpplot::setZRange (T start, T finish) {
+  pipe_ << "set zrange[" << start << ":"<< finish << "]\n";
+}
+
+template <typename T>
+void Cpplot::plot (std::vector<T> v) {
+  pipe_ << "plot '-' with points\n";
+  for (int i=0; i<v.size(); ++i) {
+    pipe_ << i << ", " << v[i] << "\n";
+  }
+  pipe_ << "e\n";
+  pipe_.flush();
+}
+
+template <typename T>
+void Cpplot::plot (std::vector<std::pair<T,T>> vpair) {
+  pipe_ << "plot '-' with points\n";
+  for (const auto& e: vpair) {
+    pipe_ << e.first << ", " << e.second << "\n";
+  }
+  pipe_ << "e\n";
+  pipe_.flush();
+}
 #endif
